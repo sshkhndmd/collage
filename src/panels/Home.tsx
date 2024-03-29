53a5731b7a4e0b9fbb34d53d6342eab08b99f8de
sshkhndmd <hosslerjx@gmail.com>
2024-03-29 22:30:18 +0500

    second

--- src/panels/Home.tsx
+++ src/panels/Home.tsx
@@ -15,6 +15,11 @@ import {
 import { UserInfo } from '@vkontakte/vk-bridge';
 import FrameImage from '../assets/frames.svg';
 import CollageImage from '../assets/collage.svg';
+import NewYear from '../assets/newYear.svg';
+import HappyBirthday from '../assets/happyBirthday.svg';
+import March from '../assets/march.svg';
+import Halloween from '../assets/halloween.svg';
+import ButtonMenu from '../assets/buttonMenu.svg';
 import './style.css';
 
 export interface HomeProps extends NavIdProps {
@@ -25,22 +30,52 @@ export const Home: FC<HomeProps> = ({ id, fetchedUser }) => {
   
 
   return (
-    <Panel id={id}>   
-      <Div className="home-container">  
-        <Div className="oval-container">  
-          <Text className="title">Collages</Text>   
-        </Div>  
-        <Div className="home-panel">  
-          <Div className="image-container">
-            <Image className="image" src={FrameImage} alt="Frame" style={{width: '150px', height:'150px'}}/>  
-            <Text className="image-text">Рамки</Text>
-          </Div>  
-          <Div className="image-container">
-            <Image className="image" src={CollageImage} alt="Collage" style={{width: '150px', height:'150px'}}/>  
-            <Text className="image-text">Коллажи</Text>
-          </Div>  
-        </Div>  
-      </Div>  
-    </Panel>     
+    <Panel id={id}>    
+  <Div className="home-container">
+     {/* ---------------------верхний блок---------------------------------- */}
+    <Div className="home">
+        <Div className="oval-container">   
+          <Text className="title">Collages</Text>    
+        </Div> 
+        <Div className="home-panel">   
+          <Div className="image-container"> 
+            <Image className="image" src={FrameImage} alt="Frame" style={{width: '150px', height:'150px'}}/>   
+            <Text className="image-text">Рамки</Text> 
+          </Div>   
+          <Div className="image-container"> 
+            <Image className="image" src={CollageImage} alt="Collage" style={{width: '150px', height:'150px'}}/>   
+            <Text className="image-text">Коллажи</Text> 
+          </Div>   
+        </Div>
+    </Div>
+    {/* ---------------------средний блок---------------------------------- */}
+    <Div>
+      <Text className="title2">Шаблоны</Text> 
+    </Div>   
+      {/* ---------------------нижний блок---------------------------------- */}
+    <Div className="template-container">
+      <Div className="template">
+        <Text className="template-text">Новый год</Text>
+        <Image className="template-image" src={NewYear} alt="Template 1" style={{width: '350px', height: '320px'}}/>
+        
+      </Div>
+      <Div className="template">
+        <Text className="template-text">День Рождения</Text>
+        <Image className="template-image" src={HappyBirthday} alt="Template 2" style={{width: '350px', height: '320px'}}/>
+        
+      </Div>
+      <Div className="template">
+        <Text className="template-text">8 марта</Text>
+        <Image className="template-image" src={March} alt="Template 3" style={{width: '350px', height: '320px'}}/>
+        
+      </Div>
+      <Div className="template">
+        <Text className="template-text">Хэллоуин</Text>
+        <Image className="template-image" src={Halloween} alt="Template 4" style={{width: '350px', height: '320px'}}/>
+        
+      </Div>
+    </Div>
+  </Div>   
+</Panel>    
   );
 };
