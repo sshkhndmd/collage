75e68dea33b0dfaf3e9d0e7e0ca4c3dd51985078
SovaaaQ <toxic_mari@mail.ru>
2024-04-15 14:24:08 +0500

    bim

--- src/constants/collageTemplates.ts
+++ src/constants/collageTemplates.ts
@@ -10,34 +10,42 @@ import Collage8 from "../assets/collage8.svg";
 export const collageTemplates = [
     {
         id: 1,
-        url: Collage1
+        url: Collage1,
+        cod: ""
     },
     {
         id: 2,
-        url: Collage2
+        url: Collage2,
+        cod: ""
     },
     {
         id: 3,
-        url: Collage3
+        url: Collage3,
+        cod: ""
     },
     {
         id: 4,
-        url: Collage4
+        url: Collage4,
+        cod: ""
     },
     {
         id: 5,
-        url: Collage5
+        url: Collage5,
+        cod: ""
     },
     {
         id: 6,
-        url: Collage6
+        url: Collage6,
+        cod: ""
     },
     {
         id: 7,
-        url: Collage7
+        url: Collage7,
+        cod: ""
     },
     {
         id: 8,
-        url: Collage8
+        url: Collage8,
+        cod: ""
     },
 ]
